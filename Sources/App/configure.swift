be5f7d3231fd345660cb86e0e5d31720731f0217
Robert <rf@thepeaklab.com>
2022-03-31 16:17:53 +0200

    Update Configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,26 +8,29 @@
 
 import Vapor
 
-
-public func configure(_ config: inout Config,
-                      _ env: inout Environment,
-                      _ services: inout Services) throws {
-
-    // Router
-    if !env.arguments.contains("generateScenarioFile") {
-        let router = EngineRouter.default()
-        try routes(router)
-        services.register(router, as: Router.self)
-    }
-
-    // Register middleware
-    var middlewares = MiddlewareConfig()
-    middlewares.use(ErrorMiddleware.self)
-    services.register(middlewares)
-
-    // Commands
-    var commandConfig = CommandConfig.default()
-    commandConfig.use(GenerateScenarioFileCommand(), as: "generateScenarioFile")
-    services.register(commandConfig)
-
+public func configure(_ app: Application) throws {
+    app.commands.use(GenerateScenarioFileCommand(), as: "generateScenarioFile")
 }
+
+//public func configure(_ config: inout Config,
+//                      _ env: inout Environment,
+//                      _ services: inout Services) throws {
+//
+//    // Router
+//    if !env.arguments.contains("generateScenarioFile") {
+//        let router = EngineRouter.default()
+//        try routes(router)
+//        services.register(router, as: Router.self)
+//    }
+//
+//    // Register middleware
+//    var middlewares = MiddlewareConfig()
+//    middlewares.use(ErrorMiddleware.self)
+//    services.register(middlewares)
+//
+//    // Commands
+//    var commandConfig = CommandConfig.default()
+//    commandConfig.use(GenerateScenarioFileCommand(), as: "generateScenarioFile")
+//    services.register(commandConfig)
+//
+//}

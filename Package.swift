e2bbc0d9297e7ee72a5556ace8d544ec0f21b7fe
Robert <rf@thepeaklab.com>
2022-03-31 12:32:21 +0200

    Update Dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Mockturtle",
     dependencies: [
         .package(url: "https://github.com/vapor/crypto.git", from: "3.3.1"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.3"),
         .package(url: "https://github.com/thepeaklab/mockturtle-parser.git", from: "0.2.0")
     ],
     targets: [
